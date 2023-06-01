e0912cbf9b8c0e671f4957c91a228f585e62ca65
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-01 17:13:27 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,10 +4,10 @@ set -o allexport; source .env; set +o allexport;
 echo "waiting...";
 sleep 45s;
 
-docker-compose exec couchbase bash -c "couchbase-cli cluster-init -c 127.0.0.1 --cluster-username ${COUCHBASE_ADMINISTRATOR_USERNAME} \
+docker-compose exec -T couchbase bash -c "couchbase-cli cluster-init -c 127.0.0.1 --cluster-username ${COUCHBASE_ADMINISTRATOR_USERNAME} \
  --cluster-password ${ADMIN_PASSWORD} --cluster-name couchbase --services data --cluster-ramsize 4096"
 
- docker-compose exec couchbase bash -c "couchbase-cli setting-alert -c 127.0.0.1 --username ${COUCHBASE_ADMINISTRATOR_USERNAME} \
+ docker-compose exec -T couchbase bash -c "couchbase-cli setting-alert -c 127.0.0.1 --username ${COUCHBASE_ADMINISTRATOR_USERNAME} \
 --password ${ADMIN_PASSWORD} --enable-email-alert 1 \
 --email-host ${EMAIL_HOST} --email-port ${EMAIL_PORT} \
 --email-recipients ${ADMIN_EMAIL} \
